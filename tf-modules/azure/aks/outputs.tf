ed1912d4edb0694f174aa8d9eeb80a64e9d3eaad
Matheus Pimenta <matheuscscp@gmail.com>
2025-06-14 19:16:19 +0100

    Enable AAD integration for AKS by default
    Signed-off-by: Matheus Pimenta <matheuscscp@gmail.com>

--- tf-modules/azure/aks/outputs.tf
+++ tf-modules/azure/aks/outputs.tf
@@ -1,6 +1,6 @@
 output "kubeconfig" {
   description = "kubeconfig of the created AKS cluster"
-  value       = var.enable_aad ? azurerm_kubernetes_cluster.this.kube_admin_config_raw : azurerm_kubernetes_cluster.this.kube_config_raw
+  value       = azurerm_kubernetes_cluster.this.kube_admin_config_raw
   sensitive   = true
 }
 
